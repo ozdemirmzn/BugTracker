917c1e981ee6c5b9685f92778660538f618456f5
Ramazan Özdemir <ozdemirmzn@gmail.com>
2021-04-24 15:52:16 -0500

    Database setup completed, base code written

---
+++ Data/TicketDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugTracker.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Data
+{
+    public class TicketDbContext : DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+
+
+        public TicketDbContext(DbContextOptions<TicketDbContext> options)
+              : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class Ticket
+    {
+        public string TicketOwner { get; set; }
+
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+       
+
+        public string TicketNumber { get; set; }
+
+
+
+        public Ticket()
+        {
+            TicketNumber = GenerateTicketNumber();
+        }
+
+        public Ticket(string ticketOwner, string description) : this()
+        {
+            TicketOwner = ticketOwner;
+            Description = description;
+
+        }
+
+
+        private String GenerateTicketNumber()
+        {
+            string generateBarcodeNum = "";
+            Random rnd = new Random();
+            int randomNum1 = rnd.Next(100, 9000);
+            int randomNum2 = rnd.Next(100, 9000);
+            return generateBarcodeNum + randomNum1 + randomNum2;
+        }
+    }
+}
